16376765369eb744f74cf7da376bfa950bd51fbd
dengjle <q18030508220@163.com>
2023-05-31 12:42:36 +0800

    new change

--- PandaClient/source/PandaClient.cpp
+++ PandaClient/source/PandaClient.cpp
@@ -1,6 +1,7 @@
 #include "PandaClient.h"
 #include "ui_PandaClient.h"
 #include <QDebug>
+#include <QFontComboBox>
 #include <QMessageBox>
 PandaClient::PandaClient(QWidget *parent)
     : QMainWindow(parent)
@@ -12,13 +13,20 @@ PandaClient::PandaClient(QWidget *parent)
     connect(TcpClient, SIGNAL(readyRead()), this, SLOT(SlotReadData()));
     connect(ui->PushButton_Send,&QPushButton::clicked,this,&PandaClient::SlotSend);
 
-    TcpClient->connectToHost("frp-bus.top", 37245);
+    TcpClient->connectToHost("frp-car.top", 37330);
     TcpClient->waitForConnected(3000);
     if (TcpClient->state() == QAbstractSocket::UnconnectedState)
     {
         QMessageBox::information(this,"提示","连接失败，请重新连接");
 
     }
+
+
+    LoginIn = new Login(TcpClient);
+//    this->show();
+    LoginIn->show();
+    connect(LoginIn, &Login::SignShowClient, this, &QMainWindow::show);
+    ColorInit();
 
 }
 
@@ -27,19 +35,51 @@ PandaClient::~PandaClient()
     delete ui;
 }
 
-void PandaClient::SlotReadData()
+void PandaClient::ColorInit()
 {
-    QByteArray buffer = TcpClient->readAll();
-    if(!buffer.isEmpty())
-    {
-        ui->TextEdit->append(buffer);
-     }
+    this->setStyleSheet("background-color: rgb(20,196,188);");
+    ui->frame_2->setStyleSheet("background-color: rgb(105,105,105);");
+    ui->listWidget->setStyleSheet("background-color: rgb(169,169,169);");
+    ui->frame_3->setStyleSheet("background-color: rgb(220,220,220);");
+}
+
+void PandaClient::FontInit()
+{
+    connect(ui->FontMode, &QFontComboBox::currentFontChanged, ui->TextEdit_Send, [=](const QFont &font){
+            ui->TextEdit_Send->setFontFamily(font.toString());
+            ui->TextEdit_Send->setFocus();
+        });
+
+
 
 }
 
+void PandaClient::SlotReadData()
+{
+//    QByteArray buffer = TcpClient->readAll();
 
-
-
+//    if(!buffer.isEmpty())
+//    {
+//        ui->TextEdit->append(buffer);
+//    }
+    {
+        QByteArray buffer = TcpClient->readAll();
+        if(buffer[0] == '0' || buffer[0] == '1' || buffer[0] == '2')
+        {
+            LoginIn->signInReceive(buffer[0]);
+        }
+        else if(buffer[0] == 's')
+        {
+            auto SendMsg = buffer.mid(2);
+            ui->TextEdit->append(SendMsg);
+        }
+        else if(buffer[0] == 'v')
+        {
+            auto ServerMsg = buffer.mid(2);
+            ui->TextEdit->append(ServerMsg);
+        }
+    }
+}
 
 void PandaClient::SlotSend()
 {
@@ -47,6 +87,15 @@ void PandaClient::SlotSend()
     QString data = ui->TextEdit_Send->toPlainText();
         if(data != "")
         {
-            TcpClient->write(data.toLatin1()); //qt5去除了.toAscii()
+            data = "s " + data;
+            TcpClient->write(data.toUtf8()); //qt5去除了.toAscii()
         }
 }
+
+void PandaClient::SlotFontMode(QFont &font)
+{
+    ui->TextEdit_Send->setFontFamily(font.toString());
+    ui->TextEdit_Send->setFocus();
+}
+
+
--- PandaClient/source/PandaClient.h
+++ PandaClient/source/PandaClient.h
@@ -5,10 +5,11 @@
 #include <QTcpSocket>
 #include <QHostAddress>
 #include <QMessageBox>
+#include "Login.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class PandaClient; }
 QT_END_NAMESPACE
-
+class Login;
 class PandaClient : public QMainWindow
 {
     Q_OBJECT
@@ -16,15 +17,18 @@ class PandaClient : public QMainWindow
 public:
     PandaClient(QWidget *parent = nullptr);
     ~PandaClient();
-
+    void ColorInit();
+    void FontInit();
 private:
     Ui::PandaClient *ui;
     QTcpSocket *TcpClient;
+    Login *LoginIn;
 
 private slots:
     void SlotReadData();
-
-
     void SlotSend();
+    void SlotFontMode(QFont &font);
+
+
 };
 #endif // PANDACLIENT_H
